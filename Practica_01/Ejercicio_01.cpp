173de866352f4125ec7d78092cd653162294faa2
sebastianbirbuet-timmy04 <sebastian.birbuet@ucb.edu.bo>
2025-08-23 18:30:13 -0400

    Add files via upload

---
+++ Practica_01/Ejercicio_01.cpp
@@ -0,0 +1,29 @@
+// Materia: Programación I, Paralelo 4
+
+// Autor: Sebastián Salvador Birbuet
+
+// Carnet: 6963195 L.P.
+
+// Carrera del estudiante: Ingeniería de Sistemas
+
+// Fecha creación: 23/08/2025
+
+// Número de ejercicio: 1
+
+#include <iostream>
+#include <string> //Nos permite incluir texto no numérico en el programa, ojo que se debe usar guiones bajos para los espacios, de lo contrario, el programa se corta.
+using namespace std;
+
+int main()
+{
+	string name, degree;
+	cout << "Por favor, ingresa tu nombre completo: ";
+	cin >> name;
+	cout << "Ahora ingresa tu carrera: ";
+	cin >> degree;
+	cout << "Hola, soy estudiante de programacion." << endl; //"endl" es la abreviatura de "Endline" que nos permite imprimir el codigo con espacios de una linea.
+	cout << name << endl;
+	cout << degree << endl;
+	
+	return 0;
+}
