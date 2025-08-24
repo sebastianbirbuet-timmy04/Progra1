10c2d63aa9ecaf99509cc0658082f3aaf226f300
sebastianbirbuet-timmy04 <sebastian.birbuet@ucb.edu.bo>
2025-08-24 19:04:11 -0400

    Add files via upload

---
+++ Practica_01/Ejercicio_10.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int score;
+	cout << "Por favor, introduce tu nota: ";
+	cin >> score;
+	
+	if (score >= 0 and score <= 100)
+	{
+		if (score >= 51 and score < 60)
+		{
+			cout << "Calificacion = C.";
+		}
+		else
+		{
+			if (score >= 60 and score < 70)
+			{
+				cout << "Calificacion = C+.";
+			}
+			else
+			{
+				if (score >= 70 and score < 80)
+				{
+					cout << "Calificacion = B.";
+				}
+				else
+				{
+					if (score >= 80 and score < 90)
+					{
+						cout << "Calificacion = B+.";
+					}
+					else
+					{
+						if (score >= 90 and score < 100)
+						{
+							cout << "Calificacion = A.";
+						}
+						else
+						{
+							if (score == 100)
+							{
+								cout << "Calificacion = A+.";
+							}
+							else
+							{ 
+								cout << "Reprobado.";
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	else
+	{
+		cout << "Ingreso invalido.";
+	}
+	
+	return 0;
+}
