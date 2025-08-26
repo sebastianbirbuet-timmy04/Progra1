25e4d59baa4473debaa45563309ebe9df02df2db
sebastianbirbuet-timmy04 <sebastian.birbuet@ucb.edu.bo>
2025-08-26 17:57:14 -0400

    Update Ejercicio_10.cpp

--- Practica_01/Ejercicio_10.cpp
+++ Practica_01/Ejercicio_10.cpp
@@ -1,3 +1,15 @@
+// Materia: Programación I, Paralelo 4
+
+// Autor: Sebastián Salvador Birbuet
+
+// Carnet: 6963195 L.P.
+
+// Carrera del estudiante: Ingeniería de Sistemas
+
+// Fecha creación: 23/08/2025
+
+// Número de ejercicio: 10
+
 #include <iostream>
 using namespace std;
 
@@ -60,3 +72,4 @@ int main()
 	
 	return 0;
 }
+
