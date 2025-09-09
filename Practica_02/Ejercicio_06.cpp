0df286e3464f4808b3880bb6040e908089727790
sebastianbirbuet-timmy04 <sebastian.birbuet@ucb.edu.bo>
2025-09-08 22:34:11 -0400

    Add files via upload

---
+++ Practica_02/Ejercicio_06.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main() 
+{
+    srand(time(0));
+
+    int n;
+    cout << "Por favor, ingresa la cantidad de numeros a generar: ";
+    cin >> n;
+
+    int count = 0;
+
+    for (int i = 0; i < n; i++) 
+	{
+        int num = rand() % 10001;
+        cout << num << endl;
+
+        int prime = 1;
+        if (num < 2) 
+		{
+            prime = 0;
+        } 
+		else 
+		{
+            for (int j = 2; j * j <= num; j++) 
+			{
+                if (num % j == 0) 
+				{
+                    prime = 0;
+                    break;
+                }
+            }
+        }
+
+        if (prime == 1) 
+		{
+            count++;
+        }
+    }
+
+    cout << "La cantidad de numeros primos generada es: " << count << endl;
+
+    return 0;
+}
+
